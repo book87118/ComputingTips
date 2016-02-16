d476c5b9c40b029412f70e69ff98e79ee32ccec7
maxlin <book87118@gmail.com>
2016-02-15 23:00:47 -0500

    for test

---
+++ app/src/main/java/ching/computingtips/test.java
@@ -0,0 +1,12 @@
+package ching.computingtips;
+
+import android.util.Log;
+
+/**
+ * Created by book871181 on 16/2/15.
+ */
+public class test {
+    public test(){
+        Log.d("test","for test");
+    }
+}
